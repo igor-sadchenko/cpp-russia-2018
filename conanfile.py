8a38dc62e03c5de0ea48b14dedd521c2b7b7c896
Igor Sadchenko <i_sadchenko@wargaming.net>
2018-04-19 18:26:59 +0300

    Fix requires for conanfile

--- conanfile.py
+++ conanfile.py
@@ -4,9 +4,10 @@ class HelloConan(ConanFile):
     description = "Demo package"
     license = "https://github.com/igor-sadchenko/cpp-russia-2018/blob/master/LICENSE"
     url = "https://github.com/igor-sadchenko/cpp-russia-2018"
+    requires = "gtest/1.8.0@lasote/stable"
     name = "hello"
     version = "0.1"
-    exports = "include/*", "src/*", "CMakeLists.txt"
+    exports = "include/*", "src/*", "CMakeLists.txt", "test/*"
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
 
